6f94547808b541e50af8615bff139417cb57a1c9
Sergio Merdani <smerdani@ikub.local>
2023-02-03 22:15:05 +0100

    Second Commit

--- script.js
+++ script.js
@@ -15,7 +15,9 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
+const scores = [0, 0];
 let currentScore = 0;
+let activePlayer = 0;
 
 //Rolling Dice Functionality
 btnRoll.addEventListener('click', function () {
@@ -24,14 +26,18 @@ btnRoll.addEventListener('click', function () {
   //2. Display Dice
   diceEl.classList.remove('hidden');
   diceEl.src = `dice-${dice}.png`;
-  console.log(dice);
+  //   console.log(dice);
   //3. Check for rolled 1:
   if (dice !== 1) {
     //Add dice to the current score
     currentScore += dice;
-    currentScorePlayer1.textContent = currentScore;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
   } else {
     //Switch to next player
-    // currentScorePlayer2.textContent = currentScore;
+    //If active player is 0 then switch to one, or vice versa.
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
   }
 });
